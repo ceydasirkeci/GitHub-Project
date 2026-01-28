7cff422e1a8e55388ea05b397c2312ce3baad39a
BilalEfeAydin <efebilalaydin@gmail.com>
2026-01-28 15:20:07 +0300

    Solution Upload

---
+++ Menu.java
@@ -0,0 +1,54 @@
+package LAb;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Menu {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+
+        // Get the array size
+        System.out.print("Enter array size: ");
+        int size = in.nextInt();
+
+        int [] array = new int[size];
+
+        // Fill the array with random integers
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(101);
+        }
+
+        boolean isFinished = false;
+
+        while (!isFinished) {
+            // Display the menu
+            System.out.println("\n---- Options Menu ----");
+            System.out.println(" 1. Find Minimum Integer ");
+            System.out.println(" 2. Find Maximum Integer ");
+            System.out.println(" 3. Find The Average and Differences of Integers ");
+            System.out.println(" 4. Find The Sum of Elements With Odd/Even Numbered Indexes ");
+            System.out.println(" 5. Exit \n");
+
+            System.out.println("Choose option no: ");
+
+            // Get the option
+            int optionNo = in.nextInt();
+
+            // Do the functionality
+            if (optionNo == 1) {
+                getMin(array);
+            } else if (optionNo == 2) {
+                getMax(array);
+            } else if (optionNo == 3) {
+                getAverage(array);
+            } else if (optionNo == 4) {
+                displayOddEvenIndexSums(array);
+            } else if (optionNo == 5) {
+                isFinished = true;
+            } else {
+                System.out.println("Try again!!!");
+            }
+        }
+    }
+}
