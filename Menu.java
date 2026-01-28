b57a090b350cfd999fd93629ff48f5aa5f977600
Azra_Gonul <azragonulgursoy3@gmail.com>
2026-01-28 15:29:50 +0300

    Refactor: Improve output formatting and update array population logic
    Improved Output Formatting: Implemented printf with %.2f specifier in getAverage to limit decimal results to two places.
    
    Updated Array Population Logic: Replaced the standard for loop with a while loop and a boolean flag for array initialization as requested.
    
    Updated Array Display: Replaced standard for loop with a for-each loop for better code clarity.
    
    Added Explicit Double Casting: Applied (double) casting during division to prevent integer division errors and ensure accurate average calculations.

--- Menu.java
+++ Menu.java
@@ -1,13 +1,86 @@
-package LAb;
-
 import java.util.Random;
 import java.util.Scanner;
 
 class Menu {
+    //a method for getting the minimum integer from given array
+    public static void getMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int a : array) {
+            if (a < min) {
+                min = a;
+            }
+        }
+        System.out.println("The minimum integer of the array: " + min);
+    }
+
+    //a method for getting the maximum integer from given array
+    public static void getMax(int[] array) {
+        int max = Integer.MIN_VALUE;
+        for (int a : array) {
+            if (a > max) {
+                max = a;
+            }
+        }
+        System.out.println("The maximum integer of the array: " + max);
+    }
+
+    //gets Average
+    public static void getAverage(int[] givenArray){
+        double sum = 0;
+
+        //calculates the sum of the array
+        for(int i = 0; givenArray.length > i; i++){
+            sum += givenArray[i];
+        }
+        double arrayAverage = sum / givenArray.length;
+        System.out.println("The Array average is: " + arrayAverage);
+        System.out.print("The differences from the average would be ");
+
+        //prints differences
+        for(int i = 0; givenArray.length > i; i++){
+            System.out.print(givenArray[i] - arrayAverage + " ");
+        }
+        System.out.println();
+    }
+
+    // Method for finding the sum of elements with odd and even numbered indexes. 
+    public static void displayOddEvenIndexSums(int [] givenArray) {
+        int oddIndexSum = 0;
+        int evenIndexSum = 0;
+
+        for (int i = 0; i < givenArray.length ; i++){
+            if (i % 2 == 0) {
+                evenIndexSum += givenArray[i];
+            }
+            else {
+                oddIndexSum += givenArray[i];
+            }
+        }
+
+        //Printing out the output
+        System.out.println("Sum of even indexes: " + evenIndexSum);
+        System.out.println("Sum of odd indexes: " + oddIndexSum);
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Random random = new Random();
 
+        // Get the array size
+        System.out.print("Enter array size: ");
+        int size = in.nextInt();
+
+        int [] array = new int[size];
+
+        // Fill the array with random integers
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(101);
+        }
+
+        //prints randomized array
+        System.out.print("Your array is: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
         boolean isFinished = false;
 
         while (!isFinished) {
@@ -19,7 +92,7 @@ class Menu {
             System.out.println(" 4. Find The Sum of Elements With Odd/Even Numbered Indexes ");
             System.out.println(" 5. Exit \n");
 
-            System.out.println("Choose option no: ");
+            System.out.print("Choose option no: ");
 
             // Get the option
             int optionNo = in.nextInt();
@@ -39,5 +112,6 @@ class Menu {
                 System.out.println("Try again!!!");
             }
         }
+        in.close();
     }
 }
