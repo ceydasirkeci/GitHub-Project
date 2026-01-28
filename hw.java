6d2066b3c27bafab7d10c97707ec705d0cb28579
Ceyda Beril Sirkeci <ceydasirkeci@gmail.com>
2026-01-28 15:14:56 +0300

    Added sum method
    Added the method which finds the sum of elements with odd and even numbered indexes.

--- hw.java
+++ hw.java
@@ -28,3 +28,22 @@ public static double getAverage(int[] givenArray){
     }
     return sum / givenArray.length;
 }
+
+// Method for finding the sum of elements with odd and even numbered indexes. 
+public static void displayOddEvenIndexSums() {
+    int oddIndexSum = 0;
+    int evenIndexSum = 0;
+
+    for (int i = 0; i < array.length ; i++){
+        if (i % 2 == 0) {
+            evenIndexSum += array[i];
+        }
+        else {
+            oddIndexSum += array[i];
+        }
+    }
+
+    //Printing out the output
+    System.out.println("Sum of even indexes: " + evenIndexSum);
+    System.out.println("Sum of odd indexes: " + oddIndexSum);
+}
